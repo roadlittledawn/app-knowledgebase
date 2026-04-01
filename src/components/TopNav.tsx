5f95d822ea50975e342295dde36e451d925d9a01
Clinton Langosch <clango@amazon.com>
2026-04-01 13:31:55 -0700

    Make logo flush left and nav items flush right in TopNav
    Remove max-width container so content spans full width.

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -57,7 +57,7 @@ export function TopNav({ className = '' }: TopNavProps) {
     <header
       className={`border-b border-[var(--color-border)] bg-[var(--color-surface)] ${className}`}
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="px-4">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <Link
